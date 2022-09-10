421ee518ed07fe23b88b91389d0984bbd371489d
Sunnyinho <sunnnny619@gmail.com>
2022-09-10 21:45:47 +0545

    completed jinja basic tutorial from dbt_labs learn

--- analysis/jinja_basics.sql
+++ analysis/jinja_basics.sql
@@ -1,18 +1,34 @@
-{% set cool_message = 'wow "cool!"' %}
-{% set another_cool_message = 'this is jinja' %}
-{% set number = 100 %}
+{%- set cool_message = 'wow "cool!"' -%}
+{%- set another_cool_message = 'this is jinja' -%}
+{%- set number = 100 -%}
 {{ cool_message }} {{another_cool_message}} I want to write jinja for {{ number/10 }} times
 
 
-{% set my_animals = [ 'Tiger', 'Lion', 'Girraf', 'Elephant', 'Mongoose' ] %}
-{% for animals in my_animals %}
+{% set my_animals = [ 'Tiger', 'Lion', 'Girraf', 'Elephant', 'Mongoose' ] -%}
+{%- for animals in my_animals  -%}
 My favourite animals are : {{animals}}
 {% endfor %}
 
-
-{% set temperature = 15 %}
+{%- set temperature = 15 -%}
 {% if temperature < 25 %}
 Time for a coffee.
-{% else %}
+{%- else -%}
 Time for a beer
 {% endif %}
+
+{% set foods = [ 'Tomato', 'Hotdog', 'Broccoli', 'Potato' ] -%}
+{%- for food in foods -%}
+    {%- if food == 'Hotdog' -%}
+        {% set food_item = 'snack' %}
+    {%- else -%}
+        {%- set food_item = 'vegetables' -%}
+    {% endif %}
+This humble {{ food }} is my favourite {{ food_item }}
+{% endfor %}
+
+{% set word_set = {
+    'name':'Ram',
+    'age': 24,
+    'address': 'Singapore'
+} -%}
+The name is {{word_set['name']}}. He lives in {{word_set['address']}} and his age is {{word_set.get('age')}}
